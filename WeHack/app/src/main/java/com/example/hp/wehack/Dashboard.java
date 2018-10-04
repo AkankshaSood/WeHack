b670ed8efb54b81f0f87ea96ebd4e5576b915381
Harshita Jain <harshitaj270@gmail.com>
2018-10-05 05:09:14 +0530

    QuestionnaireFixes

--- WeHack/app/src/main/java/com/example/hp/wehack/Dashboard.java
+++ WeHack/app/src/main/java/com/example/hp/wehack/Dashboard.java
@@ -37,6 +37,119 @@ public class Dashboard extends AppCompatActivity {
             }
         });
 
+        llbio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llChem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llComputer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llElectrician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llEmbroidery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llMehendi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llMicrosoft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llMobileComputing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llParlour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llScience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llSpa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llStitching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+        llStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Dashboard.this,ListOfVideos.class);
+                startActivity(i);
+            }
+        });
+
+
 
     }
 }
